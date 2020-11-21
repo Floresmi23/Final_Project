b652d175bb283542b1e9a16bc16be2ac5241d190
Floresmi23 <71346603+Floresmi23@users.noreply.github.com>
2020-11-20 22:45:08 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+
+function myFunction() {
+  var person = prompt("Please enter your name", "Harry Potter");
+  if (person != null) {
+    document.getElementById("demo").innerHTML =
+    "Welcome back " + person + "!" + " Hover over the images to learn more about the games.";
+  }
+}
+
